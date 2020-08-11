55618c2066679a2bf74949876caf8376d263a794
Phillip Pargmann <pargmann92@gmail.com>
2020-08-11 14:10:34 +0200

    Updates

---
+++ precache-manifest.2e4d33395829b540aaa9135fb36a0bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3aba2d87c3437ada7c99edce7be7357",
+    "url": "/daily-tasks/index.html"
+  },
+  {
+    "revision": "0018442749b1c2c2505d",
+    "url": "/daily-tasks/static/css/main.423e2225.chunk.css"
+  },
+  {
+    "revision": "3200d989545175e30d42",
+    "url": "/daily-tasks/static/js/2.14aadf0b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/daily-tasks/static/js/2.14aadf0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0018442749b1c2c2505d",
+    "url": "/daily-tasks/static/js/main.6e26ff6b.chunk.js"
+  },
+  {
+    "revision": "d8b11947cfe4e504e39e",
+    "url": "/daily-tasks/static/js/runtime-main.c3d7b753.js"
+  }
+]);
